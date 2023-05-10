09d4fa4eb5e7d193de72dae9918e22342f95e500
NameplateStore <129401204+NameplateStore@users.noreply.github.com>
2023-05-10 12:09:31 +0530

    storing Data to mongoDB

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const connecToMongo = require('./db')
 const express = require('express')
+
 connecToMongo();
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+//Available routes
+app.use('/api/auth', require('./routes/auth.js'))
+app.use('/api/notes', require('./routes/notes.js'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
