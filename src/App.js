79c0a5620638f8bcbe03dccd50ba78e22145c2cb
NameplateStore <129401204+NameplateStore@users.noreply.github.com>
2023-05-13 18:50:00 +0530

    Navbar Add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is react app</h1>
+      <>
+        <Router>
+        <Navbar></Navbar>
+          <Routes>
+            <Route exact path="/" element=<Home></Home>></Route>
+            <Route exact path="/about" element=<About></About>></Route>        
+          </Routes>
+        </Router>
+        <h1>This is react app</h1>
+      </>
     </div>
   );
 }
